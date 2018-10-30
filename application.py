e6babc6ac5ebbd55379a6c03cf971a2f672d2955
Renzil Dourado <renzil1994@gmail.com>
2018-10-30 14:08:18 -0400

    added permission list in request

--- application.py
+++ application.py
@@ -1,10 +1,17 @@
 import flask, flask.views
+from flask import request
 app = flask.Flask(__name__)
 
 
 @app.route("/", methods = ["GET", "POST"])
 def hello_world():
     return "hello_world"
+
+
+@app.route("/predict", methods = ["GET", "POST"])
+def hello_world():
+    permission_list = request.get_json()['permissionList']
+    return permission_list
 
 
 if __name__ == '__main__':
