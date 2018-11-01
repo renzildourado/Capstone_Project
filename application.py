0a2df0f21ce5f783c45534fdd68a90db6daebc25
Renzil Dourado <renzil1994@gmail.com>
2018-11-01 05:36:27 -0400

    permission list processed and sent back to app

--- application.py
+++ application.py
@@ -5,24 +5,38 @@ app = flask.Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def hello_world():
-    return "hello_world"
+    return "The Server works"
 
 
 @app.route("/predict", methods = ["GET", "POST"])
 def get_permissions():
 
     received_json_object = request.get_json()
+    permission_list_string = received_json_object["permissionList"]
 
-    permission_list = received_json_object["permissionList"]
-    permission_list = permission_list[1:len(permission_list)-1]
-    permission_list = permission_list.split(",")
-    permission_list_string = "The Permissions are:\n"
+    #Getting rid of [ and ] in the string [ "A", "B" ]
+    permission_list_string = permission_list_string[1:len(permission_list_string)-1]
+
+    #Turning the string into an actual python list
+    permission_list = process_permissions(permission_list_string)
+
+    permission_list_string_return = "The Permissions are:\n"
 
     for permission in permission_list:
-        permission_list_string += permission
-        permission_list_string += "\n"
+        permission_list_string_return += permission
+        permission_list_string_return += "\n"
 
-    return permission_list_string
+    return permission_list_string_return
+
+def process_permissions(permission_string):
+    permission_list_string = permission_string.split(",")
+    permission_list = []
+
+    for word in permission_list_string:
+        permission_list.append(word[1:len(word)-1])     #stripping "" from "ABC"
+
+    return permission_list
+
 
 
 if __name__ == '__main__':
