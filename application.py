aa4b358b170271c490d880939e639e60f25986f0
Renzil Dourado <renzil1994@gmail.com>
2018-11-01 05:11:36 -0400

    permission list received at server and sent back to app

--- application.py
+++ application.py
@@ -14,9 +14,14 @@ def get_permissions():
     received_json_object = request.get_json()
 
     permission_list = received_json_object["permissionList"]
-    print("PERMISSION LIST" +permission_list)
-    print("*", received_json_object)
-    return str(received_json_object)
+    print("PERMISSION LIST")
+    permission_list_string = "The Permissions are:\n"
+
+    for permission in permission_list:
+        permission_list_string += permission
+        permission_list_string += "\n"
+
+    return permission_list_string
 
 
 if __name__ == '__main__':
