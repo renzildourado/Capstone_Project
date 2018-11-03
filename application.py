708d05f826036b9b106de55b18e0f64afd22d710
Renzil Dourado <renzil1994@gmail.com>
2018-11-03 17:43:37 -0400

    added machine learning model

--- application.py
+++ application.py
@@ -1,3 +1,6 @@
+import csv
+import pickle
+
 import flask, flask.views
 import ast
 from flask import request
@@ -9,7 +12,7 @@ def hello_world():
     return "The Server works"
 
 
-@app.route("/predict", methods = ["GET", "POST"])
+@app.route("/predict", methods = ["GET"])
 def get_permissions():
 
     received_json_object = request.get_json()
@@ -21,14 +24,43 @@ def get_permissions():
     #Turning the string into an actual python list
     permission_list = ast.literal_eval(permission_list_string)
 
-    permission_list_string_return = "The Permissions are:\n"
+    prediction = perform_predict(permission_list)
+
+    # permission_list_stringit g_return = "The Permissions are:\n"
+    #
+    # for permission in permission_list:
+    #     permission_list_string_return += permission
+    #     permission_list_string_return += "\n"
+
+    return prediction
+
+
+def perform_predict(permission_list):
+    csv_reader = csv.reader(open("attributes.csv", "r"))
+    all_permissions_list = []
+    permission_list_dict = set()
+    predict_list = []
+
+    for line in csv_reader:
+        if len(line)>0 and line[0]!= "MALWARE_DETECTION":
+            all_permissions_list.append(line[0])
 
     for permission in permission_list:
-        permission_list_string_return += permission
-        permission_list_string_return += "\n"
+        permission_list_dict.add(permission)
 
-    return permission_list_string_return
+    for permission in all_permissions_list:
+        if permission in permission_list_dict:
+            predict_list.append(1)
+        else:
+            predict_list.append(0)
 
+    linear_regression = pickle.load(open("logistic_regression_model.sav", 'rb'))
+    prediction = linear_regression.predict([predict_list])
+
+    if prediction[0]==0:
+        return "No"
+    else:
+        return "Yes"
 
 if __name__ == '__main__':
     app.debug = True
